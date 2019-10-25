58d9b99c7a46741d372082317d22dbf771c6c4d9
Vivek Kumar <vivekjaglan@outlook.com>
2019-10-26 03:25:28 +0530

    Add files via upload

---
+++ tasks/solution.py
@@ -0,0 +1,11 @@
+# Task 1: Importing Pandas library
+
+import pandas as pd
+
+
+# Task 2: Reading CSV file into a Pandas DataFrame
+df = pd.read_csv('../data/student_db.csv')
+
+
+# Task 3: 
+grade_median = df.GRADE.median()
